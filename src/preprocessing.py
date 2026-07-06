88e017d745b0e6f94450c4c37dc9dc96e43d148a
Gustavo <gupenna1@gmail.com>
2026-07-06 13:17:41 -0300

    creating preprocessing pipeline

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn import preprocessing
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 
 # loading csv
@@ -15,18 +17,19 @@ y = df['stroke']
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.25, random_state=42)
 
-# copying and substituing dataframe columns with imputed values
-imp = SimpleImputer(strategy='mean').fit(X_train[['bmi']])
-X_imputed = imp.transform(X_train[['bmi']])
-X_train_processed = X_train.copy()
-X_train_processed[['bmi']] = X_imputed
+numeric_pipeline = Pipeline(
+    steps=[
+        ('imputer', SimpleImputer(strategy='mean')),
+        ('scaler', StandardScaler())
+        ]
+    )
 
-# scaling numeric features
-X_numeric = X_train_processed[['age', 'avg_glucose_level', 'bmi']]
-scaler = preprocessing.StandardScaler().fit(X_numeric)
-X_scaled = scaler.transform(X_numeric)
+ct = ColumnTransformer(
+    transformers=[
+        ('num', numeric_pipeline, ['age', 'avg_glucose_level', 'bmi']),
+        ('cat', OneHotEncoder(), ['gender', 'ever_married', 'work_type', 'Residence_type', 'smoking_status'])
+    ],
+    remainder='passthrough'
+)
 
-# encoding categoric variables
-X_categoric = X_train[['gender', 'ever_married', 'work_type', 'Residence_type', 'smoking_status']]
-enc = preprocessing.OneHotEncoder().fit(X_categoric)
-X_encoded = enc.transform(X_categoric)
+X_trans = ct.fit_transform(X_train)
